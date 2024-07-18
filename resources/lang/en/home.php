892a47bad735733f126d7dbaddf017c2c6420390
samuel.leal <samuel.leal@hipe.cc>
2024-07-18 12:39:38 -0300

    Files para tradção

---
+++ resources/lang/en/home.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'Criando Ideias' => 'Creating Ideas',
+    'Sobre' => 'About',
+    'Habilidades' => 'Skills',
+    'Contato' => 'Contact',
+    'Banner-title' => 'Creating Ideas',
+    'Banner-text' => 'Building the future of the web, one line of code at a time, keeping up with market trends and innovations.',
+    'Personal-cargo' => 'Fullstack Web Developer',
+    'Personal-bio' => 'Full-stack web systems developer, graduated in information systems from UTFPR, 25 years old and Brazilian',
+    'Goal-title' => 'What is the goal of this page?',
+    'Goal-text' => 'With the constant changes in the job market, it is important to always be updated and aware of new technologies. Therefore, as a way to demonstrate my skills and also to practice, I had the idea to create this page. Thus, it becomes clear what I can do according to my skills.',
+    'Experiences-title-1' => 'Academic Journey - UTFPR',
+    'Experiences-text-1' => 'During my graduation, I was able to learn programming concepts practically from scratch. During this period, I dealt with basic programming, Object-Oriented Programming, Databases, Algorithm Analysis, Image Processing, Distributed Systems, Project Management, Human Behavior, C Language, C++, Java, Python, and PHP.',
+    'Experiences-sub-title-tcc' => 'My Thesis',
+    'Experiences-text-tcc' => 'When choosing themes for my thesis, something that always caught my attention was data security, and a very current theme in the year 2022 was the LGPD. Thus, it took a few months of research on the subject until reaching the final summary outlined in the title.',
+    'Experiences-title-tcc' => 'Case Study: example of the application of anonymization in a public company based on the LGPD',
+    'Experiences-text-2' => 'During the pandemic in 2020, we started this project with the goal of building platforms that integrated chatbots with the web. Here we worked with Laravel to build the platform, but we used technologies like Dialogflow, Typebot, and others to integrate intelligent agents that could be integrated into our platforms. Thus, we were able to automate and assist our clients in the digital world.',
+    'Experiences-text-3' => 'In the second semester of 2023, I joined this company as a Web Developer and was able to assist in projects with my programming knowledge using Laravel. Here I was able to further develop my knowledge in PHP, PhpUnit, Docker, Blade, and JavaScript, as well as learn new things like creating macros for CPCL and ZPL labels, managing emails in Laravel, and Vue.js.',
+    'Atualmente' => 'currently',
+    'Contact-title' => 'Get in touch',
+    'Contact-text' => 'Send your information or get in touch and we will get back to you as soon as possible.',
+    'Enviar' => 'Send'
+];
+
+?>
---
+++ resources/lang/pt-br/home.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'Sobre' => 'Sobre',
+    'Habilidades' => 'Habilidades',
+    'Contato' => 'Contato',
+    'Banner-title' => 'Criando Ideias',
+    'Banner-text' => 'Construindo o futuro da web, uma linha de código por vez, mantendo-se atualizado com as tendências e inovações do mercado.',
+    'Personal-cargo' => 'Desenvolvedor Web Fullstack',
+    'Personal-bio' => 'Desenvolvedor de sistemas web, full-stack, formado em sistemas de informação pela UTFPR, 25 anos e brasileiro',
+    'Goal-title' => 'Qual o objetivo desta página?',
+    'Goal-text' => 'Com as constantes mudanças do mercado de trabalho, é importante estar sempre atualizado e por dentro nas novas tecnologias. Sendo assim, como um meio de demonstrar as minhas capacidades e também praticar, tive a ideia de criar esta pagina. Assim, fica claro o que eu posso fazer de acordo com minhas habilidades.',
+    'Experiences-title-1' => 'Trajetória Acadêmica - UTFPR',
+    'Experiences-text-1' => 'Durante minha graduação pude aprender os conceitos da programação praticamente do zero. Neste periodo lidei com programação basico, Orientação a Objetos, Banco de Dados, Analise de algoritmos, processamento de imagens, sistemas distribuidos, gestão de projetos, comportamento humano, Linguagem C, C++, Java, Python e php',
+    'Experiences-sub-title-tcc' => 'Meu TCC',
+    'Experiences-text-tcc' => 'Durante a escolha de temas para realizar meu tcc, algo que sempre me chamou a atenção foi na segurança de dados, e um tema muito atual ali no ano de 2022 era a LGPD. Assim, foram alguns meses de pesquisa no tema até chegar no recorte final resumido no titulo',
+    'Experiences-title-tcc' => 'Estudo de caso: exemplo de aplicação da anonimização em uma empresa pública baseado na LGPD',
+    'Experiences-text-2' => 'Durante a pandemia no ano de 2020, começamos este projeto com o objetivo de construir plataformas que integrassem chatbots com web. Aqui trabalhamos com o Laravel para construir a plataforma, mas utilizamos tecnlogias com dialog flow, typebo e outros para integras agentes inteligentes que pudessem ser integrados em nossas plataformas. Assim, conseguimos automatizar e auxiliar nossos cliente no mundo digital.',
+    'Experiences-text-3' => 'No segundo semestre de 2023, entrei nessa empresa como desenvolvedor Web e pude auxiliar em projetos com meus conhecimentos de programação usando Laravel. Aqui pude desenvolver ainda mais meus conhecimentos em Php, PhpUnit, Docker, Blade e javascript, além de aprender coisas novas como criação de macro para etiquetas CPCL e ZPL, gerenciamento de E-mails no laravel e Vue.js.',
+    'Atualmente' => 'atualmente',
+    'Contact-title' => 'Entre em contato',
+    'Contact-text' => 'Envie suas informações ou entre em contato e retornaremos assim que possivel.',
+    'Enviar' => 'Enviar'
+];
+
+?>
